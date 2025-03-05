dc613d688b4fb321b4c37fec1da5b1e9f9a3cac9
volkoivan <megavirus74@gmail.com>
2025-03-05 14:29:50 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmVersionStr = "3.20.0"
-let objectMapperVersionStr = "3.5.1"
+let realmVersionStr = "10.54.1"
+let objectMapperVersionStr = "4.2.0"
 
 let package = Package(
     name: "ObjectMapper+Realm",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v12),
         .tvOS(.v10),
         .macOS(.v10_10),
     ],
